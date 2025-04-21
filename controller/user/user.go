b81e5d8d58d8fdf1cc0661adb52efb4d42945023
Thanet Sappasit <thanet.sappasit@gmail.com>
2025-04-21 10:47:34 +0700

    GetUserByEmail

--- controller/user/user.go
+++ controller/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"backend/dto"
 	"backend/model"
 	"net/http"
 
@@ -15,9 +16,9 @@ func UserController(router *gin.Engine, db *gorm.DB, firestoreClient *firestore.
 		routes.GET("/getalluser", func(c *gin.Context) {
 			GetAllUser(c, db, firestoreClient)
 		})
-		// routes.POST("/getemail", func(c *gin.Context) {
-		// 	GetUserByEmail(c, db, firestoreClient)
-		// })
+		routes.POST("/getemail", func(c *gin.Context) {
+			GetUserByEmail(c, db, firestoreClient)
+		})
 		// routes.POST("/createaccount", func(c *gin.Context) {
 		// 	CreateAccUser(c, db, firestoreClient)
 		// })
@@ -39,6 +40,30 @@ func GetAllUser(c *gin.Context, db *gorm.DB, firestoreClient *firestore.Client) 
 	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
 		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func GetUserByEmail(c *gin.Context, db *gorm.DB, firestoreClient *firestore.Client) {
+	var emailrequest dto.GetUserByEmail
+	if err := c.ShouldBindJSON(&emailrequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	if emailrequest.Email == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email is required"})
+		return
+	}
+
+	var user model.User
+	result := db.Where("email = ?", *emailrequest.Email).First(&user)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		}
 	}
 	c.JSON(http.StatusOK, user)
 }
